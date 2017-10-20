527f18ca542ab1972daeb4a63f12ae56604f2d4b
dbenesh82 <dbenesh82@gmail.com>
2017-10-20 12:49:48 +0200

    Made nice looking platyhelminth tree

--- platyhelminthes/make_tree/make_tree.R
+++ platyhelminthes/make_tree/make_tree.R
@@ -10,8 +10,13 @@ d <- dist.ml(platy)
 nj_tree <- NJ(d)
 plot(nj_tree)
 
-up_tree <- upgma(d)
-plot(up_tree)
+# up_tree <- upgma(d)
+# plot(up_tree)
+
+nj_tree <- root(nj_tree, outgroup = c("schmidtea.mediterranea", "macrostomum.lignano"))
+
+
+write.nexus(nj_tree, file = "platyhelminthes/make_tree/platy_tree.nexus")
 
 
 # max likelihood
@@ -21,12 +26,11 @@ plot(up_tree)
 njfit2 <- pml(nj_tree, platy, model = "JTT", k=4, inv=0.2)
 
 # takes a while
-ml_fit <- optim.pml(njfit2, rearrangement = "NNI", # if rearrangement = 'stochastic', very long!
-                    optInv=TRUE, optGamma=TRUE)
-
-ml_tree <- ml_fit$tree
-ml_tree_root <- root(ml_tree, outgroup = c("schmidtea.mediterranea", "macrostomum.lignano", 
-                           "protopolystoma.xenopodis", "gyrodactylus.salaris"))
-plot(ml_tree_root)
-
-write.nexus(ml_tree_root, file = "platyhelminthes/make_tree/platy_tree.nexus")
+# ml_fit <- optim.pml(njfit2, rearrangement = "NNI", # if rearrangement = 'stochastic', very long!
+#                     optInv=TRUE, optGamma=TRUE)
+# 
+# ml_tree <- ml_fit$tree
+# ml_tree_root <- root(ml_tree, outgroup = c("schmidtea.mediterranea", "macrostomum.lignano"))
+# plot(ml_tree_root)
+# 
+# write.nexus(ml_tree_root, file = "platyhelminthes/make_tree/platy_tree.nexus")
