6a4525d2dc95d1e5c1aad70538e56665e9d37b03
dbenesh82 <dbenesh82@gmail.com>
2017-10-23 14:52:37 +0200

    all queried nem genes, aligned and saved

--- nematodes/align_sequences/align_sequences.R
+++ nematodes/align_sequences/align_sequences.R
@@ -22,7 +22,6 @@ for(o in orthologs){
   } else {
     
     al <- read.alignment(file_name, format = 'fasta')
-    print(al$nb)
     if(al$nb >= 70) { # if sequences for at least 70 worms, skip
       ortho_to_test <- c(ortho_to_test, o)
     }
@@ -36,24 +35,32 @@ for(o in orthologs){
 # loop through files, align sequences
 for(o in ortho_to_test){
   
-  # make file name string
-  file_name <- paste("nematodes/fasta_files/", o, ".txt", sep = "")
-  
-  # read in fasta in msa alignment format
-  fasta <- readAAStringSet(file = file_name)
-  # align using clustalW with default parameters
-  alignment <- msa(fasta)
-  # convert to seqinr alignment format, which is a list
-  alignment <- msaConvert(alignment, type="seqinr::alignment")
-  # clean up alignment; remove gaps, non-conserved regions
-  alignment <- cleanAlignment(alignment, 30, 30)
-  
+  # check if alignment file already exists
   file_name <- paste("nematodes/align_sequences/alignments/", o, ".txt", sep = "")
-  
-  write.fasta(sequences = as.list(alignment$seq),
-              names = alignment$nam,
-              file.out = file_name,
-              as.string=TRUE)
-  
+  if(file.exists(file_name)) {
+    next # if alignment exists, skip to next fasta file
+    
+  } else {
+    
+    # make fasta file name string to load fasta file
+    file_name <- paste("nematodes/fasta_files/", o, ".txt", sep = "")
+    # read in fasta in msa alignment format
+    fasta <- readAAStringSet(file = file_name)
+    # align using clustalW with default parameters
+    alignment <- msa(fasta)
+    # convert to seqinr alignment format, which is a list
+    alignment <- msaConvert(alignment, type = "seqinr::alignment")
+    # clean up alignment; remove gaps, non-conserved regions
+    alignment <- cleanAlignment(alignment, 30, 30)
+    # file name for writing alignment file
+    file_name <-
+      paste("nematodes/align_sequences/alignments/", o, ".txt", sep = "")
+    # write aligned fasta
+    write.fasta(
+      sequences = as.list(alignment$seq),
+      names = alignment$nam,
+      file.out = file_name,
+      as.string = TRUE
+    )
+  }
 }
-
